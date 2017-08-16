964ea861b537d11121ad5844483ff29cd7e714b4
yongyechuhan <liuxinaihui@live.com>
2017-08-16 19:33:14 +0800

    完善首页上传图片功能。

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 
 @csrf_exempt
-def index(request):
+def uploadImage(request):
     token_info = certificate.certificate()
     appId = token_info.appId
     sigurate = sign.Sign(token_info.jsapi_ticket, "http://"+request.getHost()+request.get_full_path())
@@ -14,3 +14,13 @@ def index(request):
     print "signature:" + sigurate_ret['signature']
     data = "{'timestamp':'%s','nonceStr':'%s','signature':'%s'}" % (sigurate_ret['timestamp'], sigurate_ret['nonceStr'], sigurate_ret['signature'])
     return render_to_response( 'index.html', {'sigurate_inf':json.dumps(data), 'appId':json.dumps(appId)})
+
+@csrf_exempt
+def shareImage(request):
+    token_info = certificate.certificate()
+    appId = token_info.appId
+    sigurate = sign.Sign(token_info.jsapi_ticket, "http://"+request.get_host()+request.get_full_path())
+    sigurate_ret = sigurate.sign()
+    print "signature:" + sigurate_ret['signature']
+    data = "{'timestamp':'%s','nonceStr':'%s','signature':'%s'}" % (sigurate_ret['timestamp'], sigurate_ret['nonceStr'], sigurate_ret['signature'])
+    return render_to_response( 'imageviewer.html', {'sigurate_inf':json.dumps(data), 'appId':json.dumps(appId)})
