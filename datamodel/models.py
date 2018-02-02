a4b8c19220e22112f68fe7e711019d015eb56ad5
liuxi <liuxinaihui@live.com>
2018-02-02 15:31:57 +0800

    创建新的数据表：绘画评论。

--- datamodel/models.py
+++ datamodel/models.py
@@ -17,5 +17,13 @@ class PaintingInfo(models.Model):
     def __unicode__(self):
         return u'%s %s' % (self.pic_name, self.pic_src)
 
+class PaintingComment(models.Model):
+    pic_name = models.CharField(max_length=60)
+    comment_user = models.ForeignKey(NoticerInfo)
+    comment = models.CharField(max_length=500)
+    comment_type = models.CharField(max_length=50)
+    create_time = models.DateTimeField
 
+    def __unicode__(self):
+        return u'%s %s' % (self.pic_name, self.comment_user, self.comment, self.comment_type, self.create_time)
 
